ae5251e7c5ac40de240ae84679c4ca49602d5da0
TOMLUCA7 <your-github-email@example.com>
2026-01-29 13:12:54 +0200

    Implement initial CSS styles for Tweeter interface and update main.js to include rendering of posts. Commented out test cases for post and comment functionalities for cleaner code.

--- main.js
+++ main.js
@@ -7,23 +7,27 @@ import {
   getPosts,
 } from "./model.js";
 
+import { Renderer } from "./render.js";
+
 // test all functions
 
-// Test adding a post
-addPost("This is my own post!");
-console.log(getPosts());
-// Should add: {text: "This is my own post!", id: "p3", comments: []}
+// // Test adding a post
+// addPost("This is my own post!");
+// console.log(getPosts());
+// // Should add: {text: "This is my own post!", id: "p3", comments: []}
 
-// Test removing a post
-removePost("p1");
-console.log(getPosts());
-// Should only have two posts left
+// // Test removing a post
+// removePost("p1");
+// console.log(getPosts());
+// // Should only have two posts left
 
-// Test adding comments
-addComment("p3", "Damn straight it is!");
-addComment("p2", "Second the best!");
-console.log(getPosts());
+// // Test adding comments
+// addComment("p3", "Damn straight it is!");
+// addComment("p2", "Second the best!");
+// console.log(getPosts());
 
-// Test removing comments
-removeComment("p2", "c6");
-console.log(getPosts());
+// // Test removing comments
+// removeComment("p2", "c6");
+// console.log(getPosts());
+
+Renderer.renderPosts(getPosts());
