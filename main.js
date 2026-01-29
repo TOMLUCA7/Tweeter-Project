37432acc3023f8e128e49f18fe24a103b7d9668c
TOMLUCA7 <your-github-email@example.com>
2026-01-29 12:06:17 +0200

    Add HTML structure for Tweeter interface and implement tests for post and comment functionalities in main.js

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ import {
   removePost,
   getPosts,
 } from "./model.js";
+
+// test all functions
+
+// Test adding a post
+addPost("This is my own post!");
+console.log(getPosts());
+// Should add: {text: "This is my own post!", id: "p3", comments: []}
+
+// Test removing a post
+removePost("p1");
+console.log(getPosts());
+// Should only have two posts left
+
+// Test adding comments
+addComment("p3", "Damn straight it is!");
+addComment("p2", "Second the best!");
+console.log(getPosts());
+
+// Test removing comments
+removeComment("p2", "c6");
+console.log(getPosts());
